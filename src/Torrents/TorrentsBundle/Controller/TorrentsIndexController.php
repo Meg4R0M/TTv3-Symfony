f6d14f1d980b2058867cb3d681f4e37a871cd3b8
meg4r0m <psykoterro@gmail.com>
2017-01-17 23:44:05 +0100

    Ajout d'une page de login

--- src/Torrents/TorrentsBundle/Controller/TorrentsIndexController.php
+++ src/Torrents/TorrentsBundle/Controller/TorrentsIndexController.php
@@ -3,11 +3,17 @@
 namespace Torrents\TorrentsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class TorrentsIndexController extends Controller
 {
     public function shoutboxAction()
     {
         return $this->render('TorrentsBundle:Frames:shoutbox.html.twig');
+    }
+
+    public function fullpageLoginAction()
+    {
+        return $this->render('UsersBundle:Default:login.html.twig', array('error' => null));
     }
 }
