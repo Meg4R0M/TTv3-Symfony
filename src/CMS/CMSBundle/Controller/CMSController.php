346358568c6539a6b082f865f41ef2934979f84a
psykoterro <psykoterro@gmail.com>
2017-01-24 15:38:49 +0100

    Message system OK

--- src/CMS/CMSBundle/Controller/CMSController.php
+++ src/CMS/CMSBundle/Controller/CMSController.php
@@ -3,6 +3,9 @@
 namespace CMS\CMSBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CMSController extends Controller
 {
@@ -14,5 +17,60 @@ class CMSController extends Controller
         }else{
             return $this->render('CMSBundle:Default:index.html.twig', array('user' => $usr));
         }
+    }
+
+    public function ajaxAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()){
+            $action = $request->request->get('action');
+            if ($action == 'view_unread_messages')
+            {
+                $provider = $this->container->get('fos_message.provider');
+                $threads = $provider->getInboxThreads();
+
+                return $this->render('MessageBundle:Message:inboxAjax.html.twig', array(
+                    'threads' => $threads
+                ));
+            }
+            return new Response("<hr><br /><b>".$action."</b><br /><hr>", 200);
+        }
+        return new Response("Erreur : Ce n'est pas une requete Ajax", 400);
+    }
+
+    public function ajaxMessagesAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()){
+            $action = $request->request->get('action');
+            if ($action == 'messages_new_message')
+            {
+                $form = $this->container->get('fos_message.new_thread_form.factory')->create();
+                $formHandler = $this->container->get('fos_message.new_thread_form.handler');
+
+                $do = $request->request->get('do');
+                if ($do == 'save') {
+                    $message = $formHandler->process($form);
+                    return new RedirectResponse($this->container->get('router')->generate('message_thread_view', array(
+                        'threadId' => $message->getThread()->getId(),
+                    )));
+                }
+
+                return $this->container->get('templating')->renderResponse('MessageBundle:Message:newThreadAjax.html.twig', array(
+                    'form' => $form->createView(),
+                    'data' => $form->getData(),
+                ));
+            }
+            elseif ($action == 'mark_as_unread')
+            {
+                /*$idThread = $request->request->get('id');
+                $em = $this->getDoctrine()->getManager();
+                $thread = $em->getRepository('UsersBundle:MessageMetadata')->findOneBy(array('id' => $idThread));
+                $thread->setIsRead(0);
+                $em->persist($thread);
+                $em->flush();*/
+                return new Response("<hr><br /><b>Not yet implemented !</b><br /><hr>", 200);
+            }
+            return new Response("<hr><br /><b>".$action."</b><br /><hr>", 200);
+        }
+        return new Response("Erreur : Ce n'est pas une requete Ajax", 400);
     }
 }
--- web/js/messages.js
+++ web/js/messages.js
@@ -1,4 +1,3 @@
-
 var $last_reply_id=0,$isPostingReply=false,replyEditorLoaded=false,checkedCheckBoxes=0,$allChecked=false;function _activeTinymceEditor(aEditor)
 {if(!replyEditorLoaded)
 {replyEditorLoaded=aEditor;replyEditorLoaded.val('');tinyMCE.execCommand('mceAddControl',false,'postAComment');tinymce.execCommand('mceFocus',false,'postAComment');$('.postACommentButtons').fadeIn('slow');tinyMCE.activeEditor.setContent('');}}
@@ -18,20 +17,37 @@ _activeTinymceEditor($(this));buildQuery='action=messages_get_reply&message_id='
 else
 {var $activeEditorContent=tinyMCE.activeEditor.getContent();tinyMCE.activeEditor.setContent($activeEditorContent+serverResponse);$.TSUE.autoScroller($('#postAComment'),'postAComment');}}
 else if(serverResponse)
-{$.TSUE.dialog(serverResponse);}}})});$(document).on('click','#messages_view_all',function(e)
-{e.preventDefault();$.TSUE.jumpInternal('mailbox.php');});$(document).on('click','#messages_new_message',function(e)
-{e.preventDefault();$.TSUE.insertLoaderAfter(this);buildQuery='action=messages_new_message&securitytoken='+TSUESettings['stKey'];var receiver_membername=$(this).attr('receiver_membername');if(receiver_membername)
-{buildQuery+='&receiver_membername='+$.TSUE.urlEncode(receiver_membername);}
-$.ajax
-({url:TSUESettings['website_url']+'/ajax/messages.php',data:buildQuery,success:function(serverResponse)
-{$.TSUE.dialog(serverResponse);$('#messages_post_new_message #receiver_membername').on('keyup',function()
-{$.TSUE.autoComplete(this,'search_membername');});}});});$(document).on('submit','#messages_post_message',function(e)
-{$.TSUE.insertLoaderAfter('#message_cancel');$('#serverResponse').remove();e.preventDefault();var receiver_membername=$('#receiver_membername',this).val(),subject=$('#subject',this).val(),message=($.TSUE.isTinyMCERunning()?tinyMCE.activeEditor.getContent():$('textarea[name="message"]',this).val());TSUESettings['showLoaderWhileAjax']=false,TSUESettings['closeOverlayWhileAjax']=false;buildQuery='action=messages_new_message&do=save&receiver_membername='+$.TSUE.urlEncode(receiver_membername)+'&subject='+$.TSUE.urlEncode(subject)+'&message='+$.TSUE.urlEncode(message)+'&securitytoken='+TSUESettings['stKey'];$.ajax
-({url:TSUESettings['website_url']+'/ajax/messages.php',data:buildQuery,success:function(serverResponse)
-{if(!$.TSUE.findresponsecode(serverResponse))
-{$(serverResponse).prependTo('#show_member_messages');$('#messages_no_message').remove();$.TSUE.closedialog();$.TSUE.alert(TSUEPhrases['message_posted']);}
-else
-{$('<div id="serverResponse">'+$.TSUE.clearresponse(serverResponse)+'</div>').prependTo($.TSUE.findOverlayTextDiv());}}});TSUESettings['showLoaderWhileAjax']=true,TSUESettings['closeOverlayWhileAjax']=true;return false;});$('#messages_post_reply').submit(function(e)
+{$.TSUE.dialog(serverResponse);}}})});
+$(document).on('click','#messages_view_all',function(e){
+    e.preventDefault();
+    $.TSUE.jumpInternal('/mailbox');
+});
+$(document).on('click','#messages_sent',function(e){
+    e.preventDefault();
+    $.TSUE.jumpInternal('/mailbox/sent');
+});
+$(document).on('click','#messages_deleted',function(e){
+    e.preventDefault();
+    $.TSUE.jumpInternal('/mailbox/deleted');
+});
+$(document).on('click','#messages_new_message',function(e){
+    e.preventDefault();
+    $.TSUE.insertLoaderAfter(this);
+    buildQuery='action=messages_new_message';
+    var receiver_membername=$(this).attr('receiver_membername');
+    if(receiver_membername){
+        buildQuery+='&receiver_membername='+$.TSUE.urlEncode(receiver_membername);
+    }
+    $.ajax({
+        url:TSUESettings['website_url']+'/ajax/messages',data:buildQuery,success:function(serverResponse){
+            $.TSUE.dialog(serverResponse);
+            $('#messages_post_new_message #receiver_membername').on('keyup',function(){
+                $.TSUE.autoComplete(this,'search_membername');
+            });
+        }
+    });
+});
+$('#messages_post_reply').submit(function(e)
 {e.preventDefault();$isPostingReply=true;$.TSUE.insertLoaderAfter('#post-reply-buttons');var message_id=$('input[name="message_id"]',this).val(),spanID='#message_id_'+message_id,reply=tinyMCE.activeEditor.getContent();buildQuery='action=messages_post_reply&message_id='+parseInt(message_id)+'&reply='+$.TSUE.urlEncode(reply)+'&securitytoken='+TSUESettings['stKey'];$.ajax({url:TSUESettings['website_url']+'/ajax/messages.php',data:buildQuery,success:function(serverResponse)
 {if(!$.TSUE.findresponsecode(serverResponse)&&serverResponse!='')
 {var $search_last_reply_id=serverResponse.match(/~last_reply_id:([0-9]+)~/);serverResponse=serverResponse.replace($search_last_reply_id['0'],'');$last_reply_id=parseInt($search_last_reply_id['1']);$(serverResponse).appendTo('#fetchNewMessages');$.TSUE.alert(TSUEPhrases['message_saved']);tinyMCE.activeEditor.setContent('');$isPostingReply=false;}
@@ -77,29 +93,61 @@ $(window).load(function()
 else
 {$this.html($.TSUE.clearresponse(serverResponse));}}});}
 return false;});$(document).on('click','#postAComment',function(e)
-{_activeTinymceEditor($(this));});$('#pm_markAsUnread').on('click',function(e)
-{e.preventDefault();var $this=$(this),$message_id=parseInt($this.attr('rel'));if(!$message_id)
-{$.TSUE.alert(TSUEPhrases['message_required_fields_error']);return false;}
-buildQuery='action=mark_as_unread&message_id='+$message_id+'&securitytoken='+TSUESettings['stKey'];$.ajax
-({url:TSUESettings['website_url']+'/ajax/messages.php',data:buildQuery,success:function(serverResponse)
-{if(serverResponse)
-{$.TSUE.dialog(serverResponse);}
-else
-{$.TSUE.jumpInternal('mailbox.php');}}});return false;});$('#pm_DeleteMessage').on('click',function(e)
-{e.preventDefault();var $this=$(this),$message_id=parseInt($this.attr('rel'));if(!$message_id)
-{$.TSUE.alert(TSUEPhrases['message_required_fields_error']);return false;}
-buildQuery='action=messages_delete_messages&message_ids='+$message_id+'&securitytoken='+TSUESettings['stKey'];$.ajax
-({url:TSUESettings['website_url']+'/ajax/messages.php',data:buildQuery,success:function(serverResponse)
-{if($.TSUE.findresponsecode(serverResponse)=='E')
-{$.TSUE.dialog(serverResponse);}
-else
-{$.TSUE.jumpInternal('mailbox.php');}}});});$('#pm_forwardMessage').on('click',function(e)
-{e.preventDefault();var $this=$(this),$message_id=parseInt($this.attr('rel'));if(!$message_id)
-{$.TSUE.alert(TSUEPhrases['message_required_fields_error']);return false;}
-buildQuery='action=messages_new_message&message_id='+$message_id+'&securitytoken='+TSUESettings['stKey'];$.ajax
-({url:TSUESettings['website_url']+'/ajax/messages.php',data:buildQuery,success:function(serverResponse)
-{$.TSUE.dialog(serverResponse);$('#messages_post_new_message #receiver_membername').on('keyup',function()
-{$.TSUE.autoComplete(this,'search_membername');});}});});$(window).load(function()
-{if(TSUESettings['currentActiveURL'].match(/dialog=pm/ig))
-{var $this=$('li[rel="checkMessages"]');$.TSUE.checkMessages($this);$(document).ajaxStop(function()
-{$.TSUE.dialog($('.atext',$this).html());});}});
+{_activeTinymceEditor($(this));});
+$('#pm_markAsUnread').on('click',function(e){
+    e.preventDefault();
+    var $this=$(this),$message_id=parseInt($this.attr('rel'));
+    if(!$message_id){
+        $.TSUE.alert(TSUEPhrases['message_required_fields_error']);
+        return false;
+    }
+    buildQuery='action=mark_as_unread&message_id='+$message_id+'&securitytoken='+TSUESettings['stKey'];
+    $.ajax({url:TSUESettings['website_url']+'/ajax/messages',data:buildQuery,success:function(serverResponse){
+        if(serverResponse){
+            $.TSUE.dialog(serverResponse);
+        }else{
+            $.TSUE.jumpInternal('mailbox');
+        }
+    }});
+    return false;
+});
+$('#pm_DeleteMessage').on('click',function(e){
+    e.preventDefault();
+    var $this=$(this),$message_id=parseInt($this.attr('rel'));
+    if(!$message_id){
+        $.TSUE.alert(TSUEPhrases['message_required_fields_error']);
+        return false;
+    }
+    buildQuery='action=messages_delete_messages&message_ids='+$message_id+'&securitytoken='+TSUESettings['stKey'];
+    $.ajax({url:TSUESettings['website_url']+'/ajax/messages',data:buildQuery,success:function(serverResponse){
+        if($.TSUE.findresponsecode(serverResponse)=='E'){
+            $.TSUE.dialog(serverResponse);
+        }else{
+            $.TSUE.jumpInternal('mailbox');
+        }
+    }});
+});
+$('#pm_forwardMessage').on('click',function(e){
+    e.preventDefault();
+    var $this=$(this),$message_id=parseInt($this.attr('rel'));
+    if(!$message_id){
+        $.TSUE.alert(TSUEPhrases['message_required_fields_error']);
+        return false;
+    }
+    buildQuery='action=messages_new_message&message_id='+$message_id+'&securitytoken='+TSUESettings['stKey'];
+    $.ajax({url:TSUESettings['website_url']+'/ajax/messages',data:buildQuery,success:function(serverResponse){
+        $.TSUE.dialog(serverResponse);
+        $('#messages_post_new_message #receiver_membername').on('keyup',function(){
+            $.TSUE.autoComplete(this,'search_membername');
+        });
+    }});
+});
+$(window).load(function(){
+    if(TSUESettings['currentActiveURL'].match(/dialog=pm/ig)){
+        var $this=$('li[rel="checkMessages"]');
+        $.TSUE.checkMessages($this);
+        $(document).ajaxStop(function(){
+            $.TSUE.dialog($('.atext',$this).html());
+        });
+    }
+});
